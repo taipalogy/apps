4cb0f4a7037ef888aac9e7945fed86c9a11a616e
Wes Sotron <taipalogy@gmail.com>
2023-06-08 17:59:36 +0800

    edit stem

--- stem.ts
+++ stem.ts
@@ -6,37 +6,19 @@ import { lemmatize } from '../taipa/src/unchange/lemmatizer';
 
 const stdin = process.openStdin();
 
-function getStems(literal: string, ending: string) {
-  const l = literal;
-  const ie = ending;
-  const stems: string[] = [];
-  if (l.length - ie.length != 0) {
-    stems.push(l.substring(0, l.length - ie.length));
-    return stems;
+function getStem(literal: string, ending: string) {
+  if (literal.length - ending.length != 0) {
+    return literal.substring(0, literal.length - ending.length);
   }
-  return stems;
-}
-
-function getInflectionalSuffixes(ending: string) {
-  const desinences: string[] = [];
-  if (ending) {
-    desinences.push(ending);
-    return desinences;
-  }
-  return desinences;
+  return '';
 }
 
 stdin.addListener('data', function (data) {
   if (process.argv.length == 2) {
     const lxLemma = lemmatize(data.toString().trim());
-    const stems = getStems(
-      lxLemma.word.literal,
-      lxLemma.getInflectionalEnding()
-    );
-    const inflectionalSuffixes = getInflectionalSuffixes(
-      lxLemma.getInflectionalEnding()
-    );
-    stems.map((x) => console.info('stem:: ' + x));
-    inflectionalSuffixes.map((x) => console.info('desinence: ' + x));
+    const stem = getStem(lxLemma.word.literal, lxLemma.getInflectionalEnding());
+    console.info('stem: ' + stem);
+
+    console.info('desinence: ' + lxLemma.getInflectionalEnding());
   }
 });
