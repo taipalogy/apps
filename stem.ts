6e88e3b43061a9b596568119314fbff2d54b635b
Wes Sotron <taipalogy@gmail.com>
2023-06-12 18:16:51 +0800

    edit stem

--- stem.ts
+++ stem.ts
@@ -1,25 +1,10 @@
-import { lemmatize } from '../taipa/src/unchange/lemmatizer';
+import { getStem, getDesinence } from '../taipa/src/util';
 
 /**
  * > node path/to/stem.js
  */
 
 const stdin = process.openStdin();
-
-function getStem(input: string) {
-  const lxLemma = lemmatize(input);
-  const literal = lxLemma.word.literal;
-  const ending = lxLemma.getInflectionalEnding();
-  if (literal.length - ending.length != 0) {
-    return literal.substring(0, literal.length - ending.length);
-  }
-  return '';
-}
-
-function getDesinence(input: string) {
-  const lxLemma = lemmatize(input);
-  return lxLemma.getInflectionalEnding();
-}
 
 stdin.addListener('data', function (data) {
   if (process.argv.length == 2) {
