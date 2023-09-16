3c2aa925b3c605f489d7d99af542072b6b7b169a
Wes Sotron <taipalogy@gmail.com>
2023-09-16 13:46:25 +0800

    implement transliterate

--- transliterate.ts
+++ transliterate.ts
@@ -3,32 +3,10 @@ import * as fs from 'fs';
 const stdin = process.openStdin();
 
 // Define your transList to transliterate hangul
-// prettier-ignore
-const transList: string[] = [
-  'a', 'ar', 
-  'b', 'ba', 'bar', 'bi', 'bir', 'bu', 'be', 'bo', 'bur', 'bs', 
-  'cca', 'ccar', 'cce', 'cci', 'ccir', 'cco', 'ccu', 'ccur', 
-  'ch', 'cha', 'char', 'che', 'chi', 'chir', 'cho', 'chu', 'chur', 
-  'd', 'da', 'dar', 'di', 'dir', 'du', 'de', 'do', 'dur', 
-  'e', 
-  'g', 'ga', 'gar', 'gi', 'gir', 'gu', 'ge', 'go', 'gur', 'gs', 
-  'h', 
-  'i', 'ir', 
-  'j', 'ja', 'jar', 'ji', 'jir', 'ju', 'je', 'jo', 'jur', 
-  'k', 'kk', 'ka', 'kar', 'ke', 'ki', 'kir', 'ko', 'ku', 'kur', 
-  'kka', 'kkar', 'kke', 'kki', 'kkir', 'kko', 'kku', 'kkur', 
-  'l', 'lb', 'lg', 'lh', 'lm', 'lp', 'ls', 'lt', 
-  'm', 
-  'n', 'nh', 'nj', 
-  'ng', 
-  'o', 
-  'p', 'pa', 'par', 'pe', 'pi', 'pir', 'po', 'pu', 'pur', 
-  'ppa', 'ppar', 'ppe', 'ppi', 'ppir', 'ppo', 'ppu', 'ppur', 
-  's', 'ss', 
-  't', 'ta', 'tar', 'te', 'ti', 'tir', 'to', 'tu', 'tur', 
-  'tta', 'ttar', 'tte', 'tti', 'ttir', 'tto', 'ttu', 'ttur', 
-  'u', 'ur'
-];
+
+const fileContents = fs.readFileSync(process.argv[2], 'utf-8');
+const dict: string[] = JSON.parse(fileContents) || [];
+const keys = Object.keys(dict);
 
 // Analyze a syllable and get sub-syllable members.
 function analyzeSyllable(sequence: string): string[] {
@@ -36,15 +14,15 @@ function analyzeSyllable(sequence: string): string[] {
   let longestVal = '';
 
   while (sequence.length > 0) {
-    transList.forEach((val) => {
+    keys.forEach((val) => {
       if (sequence.startsWith(val) && val.length > longestVal.length) {
         longestVal = val;
       }
     });
     if (longestVal.length == 0) break;
-    console.log('>' + sequence);
+    // console.log('>' + sequence);
     sequence = sequence.slice(longestVal.length);
-    console.log('>' + sequence);
+    // console.log('>' + sequence);
     result.push(longestVal);
     longestVal = '';
   }
@@ -62,18 +40,22 @@ if (process.argv.length == 3) {
 // Process command line input
 stdin.addListener('data', function (data) {
   if (process.argv.length == 2) {
-    const letterSequence = data.toString().trim();
-    const result = analyzeSyllable(letterSequence);
-    // Output the sub-syllable members
-    console.log(result);
+    // const letterSequence = data.toString().trim();
+    // const result = analyzeSyllable(letterSequence);
   } else if (process.argv.length == 3) {
     if (!fs.existsSync(process.argv[2])) {
       console.log('File not found');
     } else {
       const input = data.toString().trim();
-      const fileContents = fs.readFileSync(process.argv[2], 'utf-8');
-      const dict: string[] = JSON.parse(fileContents) || [];
-      const keys = Object.keys(dict);
+      const result = analyzeSyllable(input);
+
+      // Output the sub-syllable members
+      // console.log(result);
+      console.info(
+        result
+          .map((val, ind) => (ind == 0 ? dict[val][0] : dict[val][1]))
+          .join('')
+      );
     }
   } else {
     console.log('Usage: ');
