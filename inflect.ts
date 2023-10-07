1e69b93c52e372c1dc2e2cc69afe4b663ceb3e46
Wes Sotron <taipalogy@gmail.com>
2023-10-07 11:35:50 +0800

    edit inflect

--- inflect.ts
+++ inflect.ts
@@ -6,7 +6,6 @@ stdin.addListener('data', function (data) {
   if (process.argv.length == 2) {
     const lexeme = inflectDesinence(data.toString().trim());
 
-    //lexeme.getForms()[0].literal;
     lexeme.getForms().map((v) => console.info('inflected form:' + v.literal));
   }
 });
